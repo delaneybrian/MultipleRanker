0c32513ba7bca289e30578d868efb9da2e935ddf
brian.delaney <brian.delaney@banachtechnology.com>
2020-04-12 18:16:16 +0100

    small changes

--- src/MultipleRanker.Definitions/Snapshots/RankingBoardSnapshot.cs
+++ src/MultipleRanker.Definitions/Snapshots/RankingBoardSnapshot.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace MultipleRanker.Definitions.Snapshots
 {
+    [DataContract]
     public class RankingBoardSnapshot
     {
+        [DataMember]
         public Guid Id { get; set; }
 
+        [DataMember]
         public ICollection<RankingBoardParticipantSnapshot> RankingBoardParticipants { get; set; } 
             = new List<RankingBoardParticipantSnapshot>();
 
+        [DataMember]
         public long MatchUpsCompleted { get; set; }
 
+        [DataMember]
         public long NumberOfRatingsPerformed { get; set; }
 
+        [DataMember]
         public DateTime LastRatingCalculatedAt { get; set; }
     }
 }
---
+++ src/MultipleRanker.Definitions/Snapshots/RatingsSnapshot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace MultipleRanker.Definitions.Snapshots
+{
+    [DataContract]
+    public class RatingsSnapshot
+    {
+        [DataMember]
+        public Guid RatingId { get; set; }
+
+        [DataMember]
+        public Guid RatingBoardId { get; set; }
+
+        [DataMember]
+        public ICollection<RatingSnapshot> RatingSnapshots 
+            = new ICollection<RatingSnapshot>();
+    }
+}
